f4d542c7d64c30585e2261300e498bccedb29f2c
nikihosseinian <nikihosseinian@ucla.edu>
2023-03-09 15:36:25 -0800

    Basic pipelining

--- client.c
+++ client.c
@@ -185,7 +185,7 @@ int main (int argc, char *argv[])
 
     struct packet ackpkt;
     struct packet pkts[WND_SIZE];
-    // int s = 0;
+    int s = 0;
     int e = 0;
     // int full = 0;
 
@@ -211,26 +211,34 @@ int main (int argc, char *argv[])
     //       handling data loss.
     //       Only for demo purpose. DO NOT USE IT in your final submission
     while (1) {
-        while (e < WND_SIZE && !feof(fp)) {
+        while (e < WND_SIZE) {
             seqNum = (seqNum + m) % MAX_SEQN;
             m = fread(buf, 1, PAYLOAD_SIZE, fp);
-            buildPkt(&pkts[e], seqNum, 0, 0, 0, 0, 0, m, buf);
-            printSend(&pkts[e], 0);
-            sendto(sockfd, &pkts[e], PKT_SIZE, 0, (struct sockaddr*) &servaddr, servaddrlen);
-            e += 1;
+            if (m > 0) {
+                buildPkt(&pkts[e], seqNum, 0, 0, 0, 0, 0, m, buf);
+                printSend(&pkts[e], 0);
+                sendto(sockfd, &pkts[e], PKT_SIZE, 0, (struct sockaddr*) &servaddr, servaddrlen);
+                e += 1;
+            }
+            else {
+                break;
+            }
         }
-
         n = recvfrom(sockfd, &ackpkt, PKT_SIZE, 0, (struct sockaddr *) &servaddr, (socklen_t *) &servaddrlen);
         if (n > 0) {
             e -= 1;
             printRecv(&ackpkt);
-            if (feof(fp)) {
+            if (m <= 0) {
                 break;
             }
         }
     }
-    while (recvfrom(sockfd, &ackpkt, PKT_SIZE, 0, (struct sockaddr *) &servaddr, (socklen_t *) &servaddrlen) > 0) {
-        printRecv(&ackpkt);
+    while (1) {
+        n = recvfrom(sockfd, &ackpkt, PKT_SIZE, 0, (struct sockaddr *) &servaddr, (socklen_t *) &servaddrlen);
+        if (n > 0) {
+            printRecv(&ackpkt);
+            break;
+        }
     }
 
     // *** End of your client implementation ***
